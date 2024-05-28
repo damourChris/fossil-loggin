cb4dc5bc1d2c9f88e2c2ea35fd2526eb35d65e4d
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:11:39 +0200

    refactor: Update admin page layout and styles

--- pages/admin.vue
+++ pages/admin.vue
@@ -23,5 +23,36 @@ useHead({
 </script>
 
 <template>
-  <div id="nc-root" />
+  <div
+    id="nc-root"
+    class="absolute dark:bg-white w-full h-full"
+  />
 </template>
+
+<style>
+/* .css-hn3jn7-EditorContainer {
+    width: 100%;
+    min-width: 800px;
+    height: 100%;
+    position: relative;
+    top: 0px;
+    left: 0px;
+    overflow: hidden;
+    padding-top: 66px;
+    background-color: rgb(239, 240, 244);
+}
+.css-1ukgnkc-ToolbarContainer {
+    box-shadow: rgba(68, 74, 87, 0.05) 0px 2px 6px 0px, rgba(68, 74, 87, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.1) 0px 2px 54px;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    min-width: 800px;
+    z-index: 300;
+    background-color: rgb(255, 255, 255);
+    height: 66px;
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+} */
+</style>

7329e950c268668b47fc2a2029e7001fa0525ad1
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:08:20 +0200

    Minor changes

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <footer class="flex flex-col items-center gap-2 p-4">
+  <footer class="flex flex-col items-center gap-2 bg-background p-4">
     <div class="flex flex-col">
       <p>
         Fossil Tracker is a simple web application that allows you to log your fossil collection.
@@ -10,7 +10,7 @@
     </div>
     <SCSelectSeparator />
     <div class="flex items-center gap-8 text-nowrap">
-      &copy; {{ new Date().getFullYear() }} Fossil Tracker
+      &copy; {{ new Date().getFullYear() }} FossLog | Damour Chris
     </div>
   </footer>
 </template>

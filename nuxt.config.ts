1599519419f076d7f3bc5c7b53a50120d9b9da3f
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:11:53 +0200

    chore: Update eslint and nuxt.config.ts for improved code formatting and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,28 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/content'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxt/content',
+    '@nuxtjs/color-mode',
+  ],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
-    // and more...
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
   shadcn: {
-    prefix: '',
+    prefix: 'SC',
     componentDir: './components/ui',
+  },
+  alias: {
+    '~/types': '../types' },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: '',
   },
 })

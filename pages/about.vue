fffdb337728ea9120c2113f56fda0bab314a6a95
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:09:39 +0200

    feat: Add about page to the application

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col justify-center gap-6 p-6">
+    <span class="text-5xl">
+      About FossLog
+    </span>
+    <div class="flex flex-col p-4">
+      <span class="text-2xl">
+        FossLog is a simple web application that allows you to log your fossil collection.
+      </span>
+    </div>
+
+    <span class="p-2 text-3xl">
+      Collections
+    </span>
+    <div class="flex flex-col p-4">
+      <span class="text-xl">
+        You can create and view collections of fossils.
+      </span>
+    </div>
+  </div>
+</template>

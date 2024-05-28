c7af14962c3f6191cb9c154da155695e6cb35e9d
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:09:52 +0200

    refactor: Update collections page with datatable and lazy loading for images

--- pages/collections.vue
+++ pages/collections.vue
@@ -1,23 +1,34 @@
 <script lang="ts" setup>
-type Collection = {
-  id: number
-  name: string
-  description: string
-  image: string
-  products: Fossil[]
-}
+import type { Collection } from '~/types'
 
-type Fossil = {
-  id: number
-  classification: string
-  description: string
-}
+import { columns } from '~/components/collections/column'
 
-defineProps<{
-  collection: Collection
-}>()
+const { data: collections } = await useAsyncData<Collection[]>(
+  'collections',
+  () => queryContent<Collection>('/collections')
+    .find(),
+)
 </script>
 
 <template>
-  <div />
+  <div class="flex flex-col items-center gap-16 px-4 py-16">
+    <div class="flex justify-center bg-background p-8 rounded-md text-4xl ring-1 ring-slate-500">
+      Collections
+    </div>
+
+    <div
+      class="flex xl:flex-row flex-col gap-6 w-full"
+    >
+      <NuxtPage />
+    </div>
+
+    <div class="bg-background p-4 rounded-md w-full ring-1 ring-slate-500">
+      <SCDataTable
+        v-if="collections"
+        :columns="columns"
+        :data="collections"
+        search-field="title"
+      />
+    </div>
+  </div>
 </template>

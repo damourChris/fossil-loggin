7aae065940e917c7688599ce0cbedc89ed4aed33
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:11:15 +0200

    feat: Add CollectionTable component for displaying a table of collections

---
+++ components/CollectionTable.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import type { Collection } from '~/types'
+
+const { collections } = defineProps<{
+  collections: Collection[]
+}>()
+</script>
+
+<template>
+  <SCTable>
+    <SCTableCaption>A list of all the collections.</SCTableCaption>
+    <SCTableHeader>
+      <SCTableRow>
+        <SCTableHead>
+          Name
+        </SCTableHead>
+        <SCTableHead>
+          Location
+        </SCTableHead>
+        <SCTableHead class="text-right">
+          Description
+        </SCTableHead>
+      </SCTableRow>
+    </SCTableHeader>
+    <SCTableBody>
+      <SCTableRow
+        v-for="collection in collections"
+        :key="collection.id"
+      >
+        <SCTableCell>
+          <NuxtLink :to="`/collections/${collection.title}`">
+            {{ collection.name }}
+          </NuxtLink>
+        </SCTableCell>
+        <SCTableCell>
+          {{ collection.location }}
+        </SCTableCell>
+        <SCTableCell class="text-right">
+          {{ collection.description }}
+        </SCTableCell>
+      </SCTableRow>
+    </SCTableBody>
+  </SCTable>
+</template>

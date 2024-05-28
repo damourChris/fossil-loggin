ba6d767f6fce2d0d99a46001411930a861df48f0
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:08:13 +0200

    breadcrum component for render classification

---
+++ components/classification/breadcrumb.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import type { Classification } from '~/types'
+
+const { classification } = defineProps<{
+  classification: Classification
+}>()
+
+const { domain, kingdom, phylum, class: class_, order, family, genus, species } = classification
+</script>
+
+<template>
+  <SCBreadcrumb>
+    <SCBreadcrumbList>
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ domain }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ kingdom }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ phylum }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ class_ }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ order }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ family }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ genus }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+      <SCBreadcrumbSeparator />
+      <SCBreadcrumbItem>
+        <NuxtLink>
+          {{ species }}
+        </NuxtLink>
+      </SCBreadcrumbItem>
+    </SCBreadcrumbList>
+  </SCBreadcrumb>
+</template>

c8e9f91d805fa9a61855a6017095de7322db1885
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-28 20:11:21 +0200

    feat: Add CollectionCard component for displaying individual collections

---
+++ components/CollectionCard.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { columns } from './occurence/columns'
+import type { Collection } from '~/types'
+
+const { collection } = defineProps<{
+  collection: Collection
+}>()
+
+const { name, description, location, fossils } = collection
+
+// const fossilsNames = fossils.map(fossil => fossil.fossil)
+
+// Fetch Fossil fossils from nuxt content based on the fossils list
+// const { data: fossilsList } = await useAsyncData<Fossil[]>(
+//   `fossils-${title}`,
+//   () => queryContent<Fossil>('/fossils')
+//     .where({
+//       name: { $in: fossilsNames },
+//     })
+//     .find())
+</script>
+
+<template>
+  <SCCard>
+    <SCCardHeader>
+      <SCCardTitle>{{ name }}</SCCardTitle>
+      <SCCardDescription>
+        {{ location }}
+      </SCCardDescription>
+    </SCCardHeader>
+
+    <SCCardContent>
+      <div class="flex py-8">
+        {{ description }}
+      </div>
+
+      <SCCard
+        v-if="fossils"
+      >
+        <SCCardHeader>
+          <SCCardTitle>
+            Fossils
+          </SCCardTitle>
+        </SCCardHeader>
+        <SCCardContent>
+          <SCDataTable
+            :columns="columns"
+            :data="fossils"
+            search-field="name"
+            :selectable="false"
+            :sortable="false"
+          />
+        </SCCardContent>
+      </SCCard>
+    </SCCardContent>
+  </SCCard>
+</template>
